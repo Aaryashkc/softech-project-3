a3b23a6c52dc197c3163daed0b3e0746858941ba
Aaryashkc <aaryashkc123@gmail.com>
2025-06-29 16:46:16 +0545

    zustand

---
+++ frontend/src/store/useDataStore.js
@@ -0,0 +1,45 @@
+import { create } from "zustand";
+import { axiosInstance } from "../lib/axios";
+
+export const useDataStore = create((set) => ({
+  states: [],
+  districts: [],
+  allDistricts: [],
+  loading: false,
+
+  fetchStates: async () => {
+    try {
+      set({ loading: true });
+      const res = await axiosInstance.get("/data/states");
+      set({ states: res.data });
+    } catch (err) {
+      console.error("Error fetching states", err);
+    } finally {
+      set({ loading: false });
+    }
+  },
+
+  fetchDistrictsByStateId: async (stateId) => {
+    try {
+      set({ loading: true });
+      const res = await axiosInstance.get(`/data/districts/${stateId}`);
+      set({ districts: res.data });
+    } catch (err) {
+      console.error("Error fetching districts", err);
+    } finally {
+      set({ loading: false });
+    }
+  },
+
+  fetchAllDistricts: async () => {
+    try {
+      set({ loading: true });
+      const res = await axiosInstance.get("/data/districts"); 
+      set({ allDistricts: res.data });
+    } catch (err) {
+      console.error("Error fetching all districts", err);
+    } finally {
+      set({ loading: false });
+    }
+  }
+}));
