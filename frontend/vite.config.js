93a25982462c2cda9a76a03e658ef5c78166fbf3
Aaryashkc <aaryashkc123@gmail.com>
2025-07-21 11:31:54 +0545

    pushing opyimized

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,4 +7,14 @@ export default defineConfig({
   plugins: [react(),
     tailwindcss()
   ],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'react-vendor': ['react', 'react-dom', 'react-router-dom'],
+          'mui-vendor': ['@emotion/react', '@emotion/styled', '@mui/material'],
+        }
+      }
+    }
+  }
 })
